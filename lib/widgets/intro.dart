87e974ee8972ae7ed7e076055b99449e6944e39b
Prateek Thakur <prateekthakur272@gmail.com>
2023-06-10 20:27:05 +0530

    adds links to profile

--- lib/widgets/intro.dart
+++ lib/widgets/intro.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:prateekthakur/constants.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Intro extends StatelessWidget {
   const Intro({super.key});
@@ -29,10 +30,30 @@ class Intro extends StatelessWidget {
                     runSpacing: 16,
                     spacing: 16,
                     children: [
-                    IconButton(onPressed: (){}, icon: const Icon(FontAwesomeIcons.github)),
-                    IconButton(onPressed: (){}, icon: const Icon(FontAwesomeIcons.instagram)),
-                    IconButton(onPressed: (){}, icon: const Icon(FontAwesomeIcons.linkedin)),
-                    IconButton(onPressed: (){}, icon: const Icon(Icons.email)),
+                    IconButton(onPressed: ()async{
+                      final url = Uri.parse('https://github.com/prateekthakur272');
+                      if(await canLaunchUrl(url)){
+                        launchUrl(url);
+                      }
+                    }, icon: const Icon(FontAwesomeIcons.github)),
+                    IconButton(onPressed: ()async{
+                      final url = Uri.parse('https://instagram.com/prateek_._thakur?igshid=OGQ5ZDc2ODk2ZA==');
+                      if(await canLaunchUrl(url)){
+                        launchUrl(url);
+                      }
+                    }, icon: const Icon(FontAwesomeIcons.instagram)),
+                    IconButton(onPressed: ()async{
+                      final url = Uri.parse('https://www.linkedin.com/in/prateek-thakur-452127216/');
+                      if(await canLaunchUrl(url)){
+                        launchUrl(url);
+                      }
+                    }, icon: const Icon(FontAwesomeIcons.linkedin)),
+                    IconButton(onPressed: ()async{
+                      final url = Uri.parse('mailto://prateekthakur272@gmail.com');
+                      if(await canLaunchUrl(url)){
+                        launchUrl(url);
+                      }
+                    }, icon: const Icon(Icons.email)),
                   ],),
                 ],
               ),
