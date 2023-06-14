d9c84e71d82a44c84a3ae2976292b015d9f5a7f3
Prateek Thakur <prateekthakur272@gmail.com>
2023-06-14 23:52:59 +0530

    on scroll hide show fab

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:prateekthakur/widgets/about.dart';
 import 'package:prateekthakur/widgets/bottom_bar.dart';
@@ -9,84 +10,105 @@ import 'package:prateekthakur/widgets/skills.dart';
 
 import '../widgets/qualifications.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var isFabVisible = true;
+
+  @override
   Widget build(BuildContext context) {
-    var fabVisible = true;
     return Scaffold(
-      body: Center(
-        child: Row(
-          children: [
-            Expanded(
-                flex: 9,
-                child: Center(
+        body: Center(
+          child: Row(
+            children: [
+              Expanded(
+                  flex: 9,
+                  child: Center(
+                      child: NotificationListener<UserScrollNotification>(
+                    onNotification: (notification) {
+                      if(notification.direction == ScrollDirection.forward){
+                        setState(() {
+                          isFabVisible = true;
+                        });
+                      }else if(notification.direction == ScrollDirection.reverse){
+                        setState(() {
+                          isFabVisible = false;
+                        });
+                      }
+                      return true;
+                    },
                     child: NestedScrollView(
-                  headerSliverBuilder: (context, innerBoxIsScrolled) => [
-                    const SliverAppBar(
-                      floating: true,
-                      snap: true,
-                      title: Text(
-                        'Prateek Thakur',
-                        style: TextStyle(fontWeight: FontWeight.bold),
+                      headerSliverBuilder: (context, innerBoxIsScrolled) => [
+                        const SliverAppBar(
+                          floating: true,
+                          snap: true,
+                          title: Text(
+                            'Prateek Thakur',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          centerTitle: true,
+                        )
+                      ],
+                      body: ListView(
+                        padding: const EdgeInsets.symmetric(horizontal: 24),
+                        children: const [
+                          Intro(),
+                          About(),
+                          SizedBox(
+                            height: 54,
+                          ),
+                          Skills(),
+                          SizedBox(
+                            height: 54,
+                          ),
+                          Qualifications(),
+                          SizedBox(
+                            height: 54,
+                          ),
+                          Portfolio(),
+                          SizedBox(
+                            height: 54,
+                          ),
+                          DoItTogether(),
+                          SizedBox(
+                            height: 54,
+                          ),
+                          BottomBar()
+                        ],
                       ),
-                      centerTitle: true,
-                    )
-                  ],
-                  body: ListView(
-                    padding: const EdgeInsets.symmetric(horizontal: 24),
-                    children: const [
-                      Intro(),
-                      About(),
-                      SizedBox(
-                        height: 54,
-                      ),
-                      Skills(),
-                      SizedBox(
-                        height: 54,
-                      ),
-                      Qualifications(),
-                      SizedBox(
-                        height: 54,
-                      ),
-                      Portfolio(),
-                      SizedBox(
-                        height: 54,
-                      ),
-                      DoItTogether(),
-                      SizedBox(
-                        height: 54,
-                      ),
-                      BottomBar()
-                    ],
-                  ),
-                )))
-          ],
+                    ),
+                  )))
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: fabVisible? SpeedDial(
-        animatedIcon: AnimatedIcons.menu_close,
-        backgroundColor: Colors.yellow.shade800,
-        foregroundColor: Colors.white,
-        children: [
-          SpeedDialChild(
-            child: const Icon(Icons.home),
-          ),
-          SpeedDialChild(
-            child: const Icon(Icons.account_circle),
-          ),
-          SpeedDialChild(
-            child: const Icon(Icons.school),
-          ),
-          SpeedDialChild(
-            child: const Icon(Icons.apps),
-          ),
-          SpeedDialChild(
-            child: const Icon(Icons.lightbulb),
-          ),
-        ],
-      ):null
-    );
+        floatingActionButton: isFabVisible
+            ? SpeedDial(
+                animatedIcon: AnimatedIcons.menu_close,
+                backgroundColor: Colors.yellow.shade800,
+                foregroundColor: Colors.white,
+                children: [
+                  SpeedDialChild(
+                    child: const Icon(Icons.home),
+                  ),
+                  SpeedDialChild(
+                    child: const Icon(Icons.account_circle),
+                  ),
+                  SpeedDialChild(
+                    child: const Icon(Icons.school),
+                  ),
+                  SpeedDialChild(
+                    child: const Icon(Icons.apps),
+                  ),
+                  SpeedDialChild(
+                    child: const Icon(Icons.lightbulb),
+                  ),
+                ],
+              )
+            : null);
   }
 }
