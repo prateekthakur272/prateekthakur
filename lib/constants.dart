0a2a061516055b40ed4192416470f9b6c2e6bb63
Prateek Thakur <prateekthakur272@gmail.com>
2023-11-18 23:24:13 +0530

    update intro

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,7 +2,7 @@ const title = 'Prateek Thakur';
 const subtitle = 'Flutter | Android | Backend | DevOps';
 const organization = 'Student at Medi-caps University';
 const intro =
-    'Welcome to my portfolio website! I\'m Prateek Thakur, a passionate software developer with expertise in Flutter, Android development, Node.js, and Firebase. With a strong foundation in mobile app development, I bring creativity and functionality to life through my coding skills. I thrive on building user-friendly interfaces and delivering seamless experiences. As a dedicated learner, I keep up with the latest industry trends to ensure my projects are up-to-date and innovative. Feel free to explore my portfolio and witness the exciting projects I\'ve crafted using my skills. Let\'s connect and collaborate to turn your ideas into reality!';
+    'Welcome to my portfolio! I\'m Prateek Thakur, a passionate software developer with expertise in Flutter cross platform app development, Backend development using various technologies, and Firebase. With a strong foundation in app development, I bring creativity and functionality to life through my coding skills. I thrive on building user-friendly interfaces and delivering seamless experiences. As a dedicated learner, I keep up with the latest industry trends to ensure my projects are up-to-date and innovative. Feel free to explore my portfolio and witness the exciting projects I\'ve crafted using my skills. Let\'s connect and collaborate to turn your ideas into reality!';
 
 const resumeUrl =
     'https://drive.google.com/file/d/1UX1nGJr_rxK8cD_Y-2gwh3vNnPHa_jzO/view?usp=share_link';
